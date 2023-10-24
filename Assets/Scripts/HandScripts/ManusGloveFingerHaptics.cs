233e3a78265fed1c6e51f472c6643aa672c4c725
hunsri <richard.hunstock@gmx.de>
2023-10-24 17:01:02 +0200

    initial commit

---
+++ Assets/Scripts/HandScripts/ManusGloveFingerHaptics.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using Manus.Hand;
+using Manus.Haptics;
+using Manus.Utility;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ManusGloveFingerHaptics : MonoBehaviour
+{
+    private float _duration = 0.2f;
+
+    private float _intensity = 0.5f;
+    
+    [SerializeField]
+    private FingerHaptics _fingerHaptics;
+
+    private Hand _hand;
+    
+    void Start()
+    {
+        _hand = _fingerHaptics.GetComponentInParent<Hand>();
+    }
+
+    public void SendImpulse(SelectEnterEventArgs eventArgs)
+    {
+        _hand.data.SetFingerHaptic(_fingerHaptics.fingerType, _intensity);
+
+        StartCoroutine(CancelImpulseCoroutine(_fingerHaptics.fingerType, _duration));
+    }
+
+    IEnumerator CancelImpulseCoroutine(FingerType fingerType, float delay)
+    {   
+        yield return new WaitForSeconds(delay);
+
+        StopImpulse(fingerType);
+    }
+
+    private void StopImpulse(FingerType fingerType)
+    {
+        _hand.data.SetFingerHaptic(fingerType, 0);
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+<H1>KeyboardVR</H1>
+
+*Environment for typing experiments with motion capturing gloves and tactile feedback*
+
+<H2>Requirements</H2>
+
+To function, this project requires the following unity package dependencies:
+- ArchVizPRO Interior Vol.6
+- ManusCorePlugin_v1.9.0
+
+<H2>Further</H2>
+
+This project makes use of the `KeyInputVR` project.
+All necessary files are provided within this project and don't need to be imported. </br>
+The original source can be viewed here: https://github.com/hunsri/KeyInputVR
